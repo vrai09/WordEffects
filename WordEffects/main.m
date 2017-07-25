aa1ee198a08925b8ad84c0e3a6caf1513bd1849d
Varninder Rai <varninder@openmailbox.org>
2017-07-24 19:34:08 -0700

    Initial Commit

---
+++ WordEffects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Livleen Rai on 2017-07-24.
+//  Copyright © 2017 Livleen Rai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
